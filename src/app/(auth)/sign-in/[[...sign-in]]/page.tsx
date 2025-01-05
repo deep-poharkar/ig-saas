0cce1573fe04028ab8c405e1d5e09628c1241c66
deep-poharkar <deeppoharkar21@gmail.com>
2025-01-05 14:19:24 +0530

    db and auth setup done w landing
    Signed-off-by: deep-poharkar <deeppoharkar21@gmail.com>

---
+++ src/app/(auth)/sign-in/[[...sign-in]]/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { SignIn } from '@clerk/nextjs'
+
+type Props = {}
+
+const page = (props: Props) => {
+  return (
+    <SignIn/>
+  )
+}
+
+export default page
---
+++ src/app/(protected)/dashboard/page.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+type Props = {}
+
+const page = async (props: Props) => {
+  return (
+    //wip: server onboard 
+    //200 \\ 201
+    <div>page</div>
+  )
+}
+
+export default page
